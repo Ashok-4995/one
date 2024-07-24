9cdf2a4597ced4c005c16fd329457aa2d685f838
Ashok-4995 <ashokanbarasan72@gmail.com>
2024-07-24 11:01:36 +0530

    first commit

---
+++ src/main/java/com/example/mydevapp/Mydevappss1Application.java
@@ -0,0 +1,13 @@
+package com.example.mydevapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydevappss1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydevappss1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mydevapp/Mydevappss1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydevapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mydevappss1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
